1349202311aae85a5f36324464cfdfd720ac618b
Sridhar Madhugiri <sridmad@microsoft.com>
2018-07-26 16:20:14 -0700

    Update versions of third party libraries

--- win32_build/envoy_deps.cmd
+++ win32_build/envoy_deps.cmd
@@ -77,11 +77,11 @@ if not exist %ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug mkdir %ENVOY_DEPE
 if not exist %ENVOY_DEPENDENCY_ROOT%\%current_directory%\release mkdir %ENVOY_DEPENDENCY_ROOT%\%current_directory%\release
 cd %current_directory%\src
 git fetch
-git checkout 5f48a3b549b047732f2d865a09e5aee872600854
+git checkout 050da53a44dede7bda136b93a9aeef47bd91fa12
 git apply %~dp0circllhist.patch
-cl /nologo /c /DWIN32 /D_DEBUG circllhist.c /Fo:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug\
+cl /nologo /Z7 /c /DWIN32 /D_DEBUG circllhist.c /Fo:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug\
 lib /nologo /out:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug\%current_directory%.lib %ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug\circllhist.obj
-cl /nologo /c /DWIN32 /DNDEBUG /Osx circllhist.c /Fo:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\release\
+cl /nologo /Z7 /c /DWIN32 /DNDEBUG /Osx circllhist.c /Fo:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\release\
 lib /nologo /out:%ENVOY_DEPENDENCY_ROOT%\%current_directory%\release\%current_directory%.lib %ENVOY_DEPENDENCY_ROOT%\%current_directory%\release\circllhist.obj
 xcopy %ENVOY_DEPENDENCY_ROOT%\%current_directory%\debug\%current_directory%.lib %ENVOY_DEPENDENCY_ROOT%\vcpkg\installed\x64-windows-static\debug\lib /y
 xcopy %ENVOY_DEPENDENCY_ROOT%\%current_directory%\release\%current_directory%.lib %ENVOY_DEPENDENCY_ROOT%\vcpkg\installed\x64-windows-static\lib /y
@@ -98,11 +98,11 @@ if not exist %current_directory% (
 )
 cd %current_directory%
 git fetch
-git checkout 99fa1f4
+git checkout 99fa1f4be8e564e8a6b613da7fa6f46c9edafc6c
 %PROTOC% %PROTOC_FLAGS% --proto_path=. --cpp_out=%PROTOC_OUTPUT%\%current_directory% metrics.proto
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\metrics.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\metrics.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\metrics.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\metrics.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\metrics.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\metrics.pb.obj
 set PROMETHEUS_PROTO_PATH=%cd%
 
@@ -118,12 +118,12 @@ if not exist %current_directory% (
 )
 cd %current_directory%
 git fetch
-git checkout d0bcc72
+git checkout ab82e5fdec8267dc2a726544b10af97675970847
 cd opencensus\proto\trace
 %PROTOC% %PROTOC_FLAGS% --proto_path=. --cpp_out=%PROTOC_OUTPUT%\%current_directory% trace.proto
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\trace.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\trace.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\trace.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\trace.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\trace.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\trace.pb.obj
 set OPENCENSUS_PROTO_PATH=%cd%
 
@@ -138,11 +138,11 @@ if not exist %current_directory% (
 )
 cd %current_directory%
 git fetch
-git checkout 9f600c2cd2d7031fdc8e25e1c9f5ad81c8cab4fe
+git checkout f9d2b11e44149635b23a002693b76512b01ae515
 %PROTOC% %PROTOC_FLAGS% --proto_path=. --cpp_out=%PROTOC_OUTPUT%\%current_directory% validate\validate.proto
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\validate\validate.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\validate\validate.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\validate.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\validate\validate.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\validate\validate.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\validate.pb.obj
 set GEN_VALIDATE_PROTO_PATH=%cd%
 
@@ -159,9 +159,9 @@ cd %current_directory%
 git fetch
 git checkout 1adfc126b41513cc696b209667c8656ea7aac67c
 %PROTOC% %PROTOC_FLAGS% --proto_path=. --cpp_out=%PROTOC_OUTPUT%\%current_directory% gogoproto\gogo.proto
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\gogoproto\gogo.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\gogoproto\gogo.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\gogo.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\gogoproto\gogo.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\gogoproto\gogo.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
 lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\gogo.pb.obj
 set GOGO_PROTO_PATH=%cd%
 
@@ -175,17 +175,17 @@ if not exist %current_directory% (
     git clone https://github.com/googleapis/googleapis.git
 )
 cd %current_directory%
-git checkout 5c6df0cd18c6a429eab739fb711c27f6e1393366
+git checkout d642131a6e6582fc226caf9893cb7fe7885b3411
 for /R %%i in (google\api\*.proto) do (
     %PROTOC% %PROTOC_FLAGS% --proto_path=%cd% --cpp_out=%PROTOC_OUTPUT%\%current_directory% "%%i"
 )
 for /R %%i in (google\rpc\*.proto) do (
     %PROTOC% %PROTOC_FLAGS% --proto_path=%cd% --cpp_out=%PROTOC_OUTPUT%\%current_directory% "%%i"
 )
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\google\api\annotations.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\api\http.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\rpc\status.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
-lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\annotations.pb.obj %PROTOC_OUTPUT%\%current_directory%\debug\http.pb.obj %PROTOC_OUTPUT%\%current_directory%\debug\status.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\google\api\annotations.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\api\http.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\rpc\status.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
-lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\annotations.pb.obj %PROTOC_OUTPUT%\%current_directory%\release\http.pb.obj %PROTOC_OUTPUT%\%current_directory%\release\status.pb.obj
+cl /nologo /Z7 /c /EHsc /Z7 /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\google\api\annotations.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\api\http.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\rpc\status.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\
+lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\debug\http.pb.obj %PROTOC_OUTPUT%\%current_directory%\debug\annotations.pb.obj %PROTOC_OUTPUT%\%current_directory%\debug\status.pb.obj
+cl /nologo /Z7 /c /EHsc /Z7 /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\google\api\annotations.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\api\http.pb.cc %PROTOC_OUTPUT%\%current_directory%\google\rpc\status.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\
+lib /nologo /out:%PROTOC_OUTPUT%\lib\%current_directory%.lib %PROTOC_OUTPUT%\%current_directory%\release\http.pb.obj %PROTOC_OUTPUT%\%current_directory%\release\annotations.pb.obj %PROTOC_OUTPUT%\%current_directory%\release\status.pb.obj
 set GOOGLE_APIS_PROTO=%cd%
 
 
@@ -212,7 +212,7 @@ for /R %%i in (*.proto) do (
 cd %PROTOC_OUTPUT%\%current_directory%\
 del trace_service.pb.cc /s
 for /R %%i in (*.cc) do (
-    cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG /Fo:%%~dpi /I %PROTOC_OUTPUT%\%current_directory% ^
+    cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /D_DEBUG /Fo:%%~dpi /I %PROTOC_OUTPUT%\%current_directory% ^
         /I %PROTOC_OUTPUT%\prometheus ^
         /I %PROTOC_OUTPUT%\opencensus-proto ^
         /I %PROTOC_OUTPUT%\protoc-gen-validate ^
@@ -225,7 +225,7 @@ lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\%current_directory%.lib @lib_debug.rs
 
 del /s /q *.obj
 for /R %%i in (*.cc) do (
-    cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Fo:%%~dpi /I %PROTOC_OUTPUT%\%current_directory% ^
+    cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\%current_directory% /DNDEBUG /Fo:%%~dpi /I %PROTOC_OUTPUT%\%current_directory% ^
         /I %PROTOC_OUTPUT%\prometheus ^
         /I %PROTOC_OUTPUT%\opencensus-proto ^
         /I %PROTOC_OUTPUT%\protoc-gen-validate ^
@@ -246,9 +246,9 @@ if not exist %PROTOC_OUTPUT%\%current_directory%\release mkdir %PROTOC_OUTPUT%\%
     --proto_path=%ENVOY_DEPENDENCY_ROOT%\vcpkg\buildtrees\grpc\src\v1.12.0-73cc223062 ^
     %ENVOY_DEPENDENCY_ROOT%\vcpkg\buildtrees\grpc\src\v1.12.0-73cc223062\src\proto\grpc\health\v1\health.proto ^
     --cpp_out=%PROTOC_OUTPUT%\health
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\health /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\health.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\ /I %PROTOC_OUTPUT%\%current_directory%
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\health /D_DEBUG %PROTOC_OUTPUT%\%current_directory%\health.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\debug\ /I %PROTOC_OUTPUT%\%current_directory%
 lib /nologo /out:%PROTOC_OUTPUT%\lib\debug\health.lib %PROTOC_OUTPUT%\%current_directory%\debug\health.pb.obj
-cl /nologo /c /EHsc /I %PROTOC_OUTPUT%\health /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\health.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\ /I %PROTOC_OUTPUT%\%current_directory%
+cl /nologo /Z7 /c /EHsc /I %PROTOC_OUTPUT%\health /DNDEBUG /Osx %PROTOC_OUTPUT%\%current_directory%\health.pb.cc /Fo:%PROTOC_OUTPUT%\%current_directory%\release\ /I %PROTOC_OUTPUT%\%current_directory%
 lib /nologo /out:%PROTOC_OUTPUT%\lib\health.lib %PROTOC_OUTPUT%\%current_directory%\release\health.pb.obj
 
 endlocal
